02d39b3bd537ca077d057a7c222e97cd9b7b1cb6
KReguieg <Khaled.Reguieg@gmail.com>
2015-03-10 16:46:17 +0100

    [TOOLBAR] Adding toolbar and different colors

--- app/src/main/java/de/flowment/flowmentapp/flowFeedActivity.java
+++ app/src/main/java/de/flowment/flowmentapp/flowFeedActivity.java
@@ -2,16 +2,22 @@ package de.flowment.flowmentapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class flowFeedActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flow_feed);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 
 
